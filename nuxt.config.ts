f318325f31aad7dd3e6f9046ea5e66d11fb83beb
Lars Wetendorff Nielsen <larswn@me.com>
2022-12-23 20:33:22 +0100

    feat: change build to static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/image-edge'],
-  css: ['@/assets/css/main.css'],
+  ssr: false,
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image-edge"],
+  css: ["@/assets/css/main.css"],
   app: {
     head: {
-      htmlAttrs: { lang: 'en' },
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      title: 'BMI Udregner',
+      htmlAttrs: { lang: "da" },
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "BMI Udregner",
       meta: [
-        { name: 'description', content: 'Beregn dit Body Mass Index (MBI).' },
+        { name: "description", content: "Beregn dit Body Mass Index (MBI)." },
       ],
     },
   },
-})
+});
